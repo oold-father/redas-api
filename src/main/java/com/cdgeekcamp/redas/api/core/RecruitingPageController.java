c3294092983a62e39087e03ed34f495927806076
Fifi Lyu <fifilyu@gmail.com>
2019-09-28 11:44:36 +0800

    使用redas.lib.core简化配置

--- src/main/java/com/cdgeekcamp/redas/api/core/RecruitingPageController.java
+++ src/main/java/com/cdgeekcamp/redas/api/core/RecruitingPageController.java
@@ -1,13 +1,14 @@
 package com.cdgeekcamp.redas.api.core;
 
+import com.cdgeekcamp.redas.lib.core.RedasMqConfig;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,19 +22,20 @@ public class RecruitingPageController {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private RedasMqConfig redasMqConfig;
+
     @PostMapping(value = "/add_recruiting_page", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    @ResponseBody
+    @ResponseBody  // 对象转换为JSON返回
     public RecruitingPageApiMessage addRecruitingPage(@RequestBody RecruitingPage recruitingPage) throws JsonProcessingException {
         String json_msg = objectMapper.writeValueAsString(recruitingPage);
         Properties p = new Properties();
-        p.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.2.2:9092");
+        p.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, redasMqConfig.getHost());
         p.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         p.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
-        JSONObject result = new JSONObject();
-
         try (KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(p)) {
-            ProducerRecord<String, String> record = new ProducerRecord<>("test", json_msg);
+            ProducerRecord<String, String> record = new ProducerRecord<>(redasMqConfig.getTopic(), json_msg);
             kafkaProducer.send(record);
             System.out.println("消息发送成功:" + json_msg);
         }
--- src/main/java/com/cdgeekcamp/redas/api/core/RedasApiCoreApplication.java
+++ src/main/java/com/cdgeekcamp/redas/api/core/RedasApiCoreApplication.java
@@ -2,8 +2,14 @@ package com.cdgeekcamp.redas.api.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
+@ComponentScan({
+        "com.cdgeekcamp.redas.lib.core",
+        "com.cdgeekcamp.redas.api.core"
+})
 public class RedasApiCoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(RedasApiCoreApplication.class);
