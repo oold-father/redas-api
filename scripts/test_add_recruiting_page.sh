bf3f8e436e573f37f6cac60cf71e6dab822787c2
Fifi Lyu <fifilyu@gmail.com>
2019-10-04 10:51:50 +0800

    增加测试脚本

---
+++ scripts/test_add_recruiting_page.sh
@@ -0,0 +1,25 @@
+curl -H "Content-Type: application/json" -X POST http://localhost:8080/add_recruiting_page -d '
+{
+"srcName": "拉勾",
+"srcPosId": "1787334",
+"srcUrl": "https://www.lagou.com/jobs/1787334.html",
+"advantage": ["五险一金", "团建"],
+"companyMainPage": "https://www.lagou.com/gongsi/420697.html",
+"companyName": "成都玛尔斯科技有限公司1234",
+"companyNature": ["移动互联网", "社交"],
+"edu": "本科及以上",
+"exp": "经验3-5年",
+"hrName": "黄小姐",
+"hrPosition": "招聘专员",
+"location": "成都",
+"salary": "14k-18k",
+"posDesc": "职位描述：\n1.负责部门产品系统的服务器端架构，开发与维护工作\n2.参与产品设计，与产品，运营和前端工程师一同探讨技术方案\n3.持续改进现有代码以应对日益增长的多样化需求\n岗位要求：\n1、3年以上python项目开发经验，计算机相关专业背景\n2、学习能力强，逻辑思维敏锐，积极主动\n3、数据结构，算法基础扎实\n4、熟悉Django，drf，对前端知识有一定了解\n5、熟悉mysql，redis\n6、熟悉Linux的使用和管理\n7. 熟悉Git",
+"position": "Python开发工程师",
+"scale": "15-50人",
+"stage": "未融资",
+"tagList": ["证券/期货", "基金", "Python", "MySQL", "区块链", "前端开发"],
+"publishTime": "2019-09-28 17:00:10",
+"spiderUuid": "afe737ef-2e14-472e-80d7-af9f9f8c5cab"
+}
+'
+echo
