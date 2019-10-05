3a605ce7deb12833bc8fb4dddb3097d8c195a67d
Fifi Lyu <fifilyu@gmail.com>
2019-10-05 22:20:05 +0800

    增加JPA路径扫描

--- src/main/java/com/cdgeekcamp/redas/api/core/RedasApiCoreApplication.java
+++ src/main/java/com/cdgeekcamp/redas/api/core/RedasApiCoreApplication.java
@@ -2,9 +2,13 @@ package com.cdgeekcamp.redas.api.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.cdgeekcamp.redas.db.model")
+@EntityScan("com.cdgeekcamp.redas.db.model")
 @ComponentScan({
         "com.cdgeekcamp.redas.lib.core",
         "com.cdgeekcamp.redas.api.core"
