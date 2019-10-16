7201ad8ddb008459381247ed4e0dcc05ae82a5a7
wjcml <37792055+wjcml@users.noreply.github.com>
2019-10-16 16:25:02 +0800

    修改职位URL生产者的类名称

--- src/main/java/com/cdgeekcamp/redas/api/core/service/PositionsAndPosUrlAndDetailProductor.java
+++ src/main/java/com/cdgeekcamp/redas/api/core/service/PositionsAndPosUrlAndDetailProductor.java
@@ -10,14 +10,16 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
 import java.util.Properties;
 
 @Service
 public class PositionDetailAndPositionsProductor {
 
-    public ApiResponse productor(PosDetailAndPositionsHtml posDetailAndPositionsHtml, String host, String topic){
-        PosDetailAndPositionsHtmlJson posDetailAndListJson = new PosDetailAndPositionsHtmlJson();
-        String jsonString = posDetailAndListJson.toJson(posDetailAndPositionsHtml);
+    public ApiResponse productor(Map map, String host, String topic){
+        Gson gson = new Gson();
+        String jsonString = gson.toJson(map);
+
         Properties p = new Properties();
         p.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, host);
         p.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
@@ -27,23 +29,6 @@ public class PositionDetailAndPositionsProductor {
             ProducerRecord<String, String> record = new ProducerRecord<>(topic, jsonString);
             kafkaProducer.send(record);
             System.out.println("消息发送成功:" + jsonString);
-        }
-
-        return new ApiResponse(ResponseCode.SUCCESS, "上报成功");
-    }
-
-    public ApiResponse positionUrlProductor(PositionUrl positionUrl, String host, String topic){
-        Gson gson = new Gson();
-        String positionUrlString = gson.toJson(positionUrl);
-        Properties p = new Properties();
-        p.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, host);
-        p.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        p.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-
-        try (KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(p)) {
-            ProducerRecord<String, String> record = new ProducerRecord<>(topic, positionUrlString);
-            kafkaProducer.send(record);
-            System.out.println("消息发送成功:" + positionUrlString);
         }
 
         return new ApiResponse(ResponseCode.SUCCESS, "上报成功");
