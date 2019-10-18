abe7229239cbf47dc91a15fb802e7a3ffeb51753
yinxin <yinxin666ccyy@gmail.com>
2019-10-18 15:24:56 +0800

    根据lib规范了变量名，删除了无效代码

--- src/main/java/com/cdgeekcamp/redas/api/core/controller/MqController.java
+++ src/main/java/com/cdgeekcamp/redas/api/core/controller/MqController.java
@@ -1,12 +1,12 @@
 package com.cdgeekcamp.redas.api.core.controller;
 
 
-import com.cdgeekcamp.redas.api.core.controller.json.MqJson;
 import com.cdgeekcamp.redas.api.core.service.PositionDetailHtmlProducer;
 import com.cdgeekcamp.redas.api.core.service.PositionUrlProducer;
 import com.cdgeekcamp.redas.api.core.service.PositionsUrlHtmlProducer;
 import com.cdgeekcamp.redas.lib.core.api.ApiResponse;
-import com.cdgeekcamp.redas.lib.core.api.receivedParameter.DataToMq;
+import com.cdgeekcamp.redas.lib.core.api.receivedParameter.HtmlToMq;
+import com.cdgeekcamp.redas.lib.core.api.receivedParameter.UrlToMq;
 import com.cdgeekcamp.redas.lib.core.jsonObject.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -25,21 +25,21 @@ public class MqController {
     private PositionDetailHtmlProducer positionDetailHtmlProducer;
 
     @PostMapping(value = "addPositionUrl")
-    public ApiResponse mqAddPositionUrl(@RequestBody MqJson mqJson) {
-        return positionUrlProducer.producerHandle(mqJson.getMsg());
+    public ApiResponse mqAddPositionUrl(@RequestBody UrlToMq urlToMq) {
+        return positionUrlProducer.producerHandle(urlToMq.getUrl());
     }
 
     @PostMapping(value = "/addPositionsUrlHtml", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ApiResponse mqAddPositionsUrlHtml(@RequestBody DataToMq dataToMq) {
-        JsonObject<DataToMq> RecrPageJson = new JsonObject();
-        String data = RecrPageJson.toJson(dataToMq);
+    public ApiResponse mqAddPositionsUrlHtml(@RequestBody HtmlToMq htmlToMq) {
+        JsonObject<HtmlToMq> htmlJson = new JsonObject();
+        String data = htmlJson.toJson(htmlToMq);
         return positionUrlProducer.producerHandle(data);
     }
 
     @PostMapping(value = "/addPositionDetailHtml", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ApiResponse mqAddPositionDetailHtml(@RequestBody DataToMq dataToMq) {
-        JsonObject<DataToMq> RecrPageJson = new JsonObject();
-        String data = RecrPageJson.toJson(dataToMq);
+    public ApiResponse mqAddPositionDetailHtml(@RequestBody HtmlToMq htmlToMq) {
+        JsonObject<HtmlToMq> htmlJson = new JsonObject();
+        String data = htmlJson.toJson(htmlToMq);
         return positionUrlProducer.producerHandle(data);
     }
 }
