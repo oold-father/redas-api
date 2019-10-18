4b0316496a2093c05f3c1e9976d5b6c24399ab3f
yinxin <yinxin666ccyy@gmail.com>
2019-10-18 10:43:02 +0800

    规范命名

--- src/main/java/com/cdgeekcamp/redas/api/core/controller/PositionUrlController.java
+++ src/main/java/com/cdgeekcamp/redas/api/core/controller/PositionUrlController.java
@@ -1,6 +1,6 @@
 package com.cdgeekcamp.redas.api.core.controller;
 
-import com.cdgeekcamp.redas.api.core.service.PositionsAndPosUrlAndDetailProductor;
+import com.cdgeekcamp.redas.api.core.service.PositionsAndPosUrlAndDetailProducer;
 import com.cdgeekcamp.redas.lib.core.*;
 import com.cdgeekcamp.redas.lib.core.api.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,30 +10,28 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @RestController
 @RequestMapping(value = "/position")
 public class PositionUrlController {
     @Autowired
-    private PositionsAndPosUrlAndDetailProductor positionsAndPosUrlAndDetailProductor;
+    private PositionsAndPosUrlAndDetailProducer positionsAndPosUrlAndDetailProducer;
 
     @PostMapping(value = "/positions_url_html", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ApiResponse positionsUrlHtml(@RequestBody PositionsUrlHtml positionsUrlHtml) {
 
-        return positionsAndPosUrlAndDetailProductor.PositionsUrlHtmlProductor(positionsUrlHtml);
+        return positionsAndPosUrlAndDetailProducer.PositionsUrlHtmlProducer(positionsUrlHtml);
     }
 
     @PostMapping(value = "/url", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ApiResponse position_url(@RequestBody PositionUrl positionUrl) {
 
-        return positionsAndPosUrlAndDetailProductor.PositionUrlProductor(positionUrl);
+        return positionsAndPosUrlAndDetailProducer.PositionUrlProducer(positionUrl);
     }
 
     @PostMapping(value = "/detail_html", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ApiResponse positionDetail(@RequestBody PositionDetailHtml positionDetailHtml) {
 
-        return positionsAndPosUrlAndDetailProductor.PositionDetailHtmlProductor(positionDetailHtml);
+        return positionsAndPosUrlAndDetailProducer.PositionDetailHtmlProducer(positionDetailHtml);
     }
 
 }
---
+++ src/main/java/com/cdgeekcamp/redas/api/core/service/PositionsAndPosUrlAndDetailProducer.java
@@ -0,0 +1,77 @@
+package com.cdgeekcamp.redas.api.core.service;
+
+import com.cdgeekcamp.redas.lib.core.*;
+import com.cdgeekcamp.redas.lib.core.api.ApiResponse;
+import com.cdgeekcamp.redas.lib.core.api.ResponseCode;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Properties;
+
+@Service
+public class PositionsAndPosUrlAndDetailProducer {
+    @Autowired
+    private PositionsUrlHtmlMqConfig positionsUrlHtmlMqConfig;
+    @Autowired
+    private PositionUrlMqConfig positionUrlMqConfig;
+    @Autowired
+    private PositionDetailHtmlMqConfig positionDetailHtmlMqConfig;
+
+    public ApiResponse PositionsUrlHtmlProducer(PositionsUrlHtml positionsUrlHtml){
+        PositionsUrlHtmlJson positionsUrlHtmlJson = new PositionsUrlHtmlJson();
+        String jsonString = positionsUrlHtmlJson.toJson(positionsUrlHtml);
+
+        Properties p = new Properties();
+        p.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, positionsUrlHtmlMqConfig.getHost());
+        p.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        p.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        try (KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(p)) {
+            ProducerRecord<String, String> record = new ProducerRecord<>(positionsUrlHtmlMqConfig.getTopic(), jsonString);
+            kafkaProducer.send(record);
+            System.out.println("消息发送成功:" + jsonString);
+        }
+
+        return new ApiResponse(ResponseCode.SUCCESS, "上报成功");
+    }
+
+    public ApiResponse PositionUrlProducer(PositionUrl positionUrl){
+        PositionUrlJson positionUrlJson = new PositionUrlJson();
+        String jsonString = positionUrlJson.toJson(positionUrl);
+
+        Properties p = new Properties();
+        p.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, positionUrlMqConfig.getHost());
+        p.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        p.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        try (KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(p)) {
+            ProducerRecord<String, String> record = new ProducerRecord<>(positionUrlMqConfig.getTopic(), jsonString);
+            kafkaProducer.send(record);
+            System.out.println("消息发送成功:" + jsonString);
+        }
+
+        return new ApiResponse(ResponseCode.SUCCESS, "上报成功");
+    }
+
+    public ApiResponse PositionDetailHtmlProducer(PositionDetailHtml positionDetailHtml){
+        PositionDetailHtmlJson positionDetailHtmlJson = new PositionDetailHtmlJson();
+        String jsonString = positionDetailHtmlJson.toJson(positionDetailHtml);
+
+        Properties p = new Properties();
+        p.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, positionDetailHtmlMqConfig.getHost());
+        p.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        p.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        try (KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(p)) {
+            ProducerRecord<String, String> record = new ProducerRecord<>(positionDetailHtmlMqConfig.getTopic(), jsonString);
+            kafkaProducer.send(record);
+            System.out.println("消息发送成功:" + jsonString);
+        }
+
+        return new ApiResponse(ResponseCode.SUCCESS, "上报成功");
+    }
+}
