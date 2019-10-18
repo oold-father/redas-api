51df73ae75c1a026c6182b9ffb82a1f82c2b2378
oold-father <yinxin666ccyy@gmail.com>
2019-10-18 15:14:13 +0800

    完成向mq发送数据的接口

--- src/main/java/com/cdgeekcamp/redas/api/core/controller/MqController.java
+++ src/main/java/com/cdgeekcamp/redas/api/core/controller/MqController.java
@@ -6,8 +6,10 @@ import com.cdgeekcamp.redas.api.core.service.PositionDetailHtmlProducer;
 import com.cdgeekcamp.redas.api.core.service.PositionUrlProducer;
 import com.cdgeekcamp.redas.api.core.service.PositionsUrlHtmlProducer;
 import com.cdgeekcamp.redas.lib.core.api.ApiResponse;
-import com.cdgeekcamp.redas.lib.core.api.ResponseCode;
+import com.cdgeekcamp.redas.lib.core.api.receivedParameter.DataToMq;
+import com.cdgeekcamp.redas.lib.core.jsonObject.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -22,9 +24,22 @@ public class MqController {
     @Autowired
     private PositionDetailHtmlProducer positionDetailHtmlProducer;
 
-    @PostMapping(value = "addUrl")
-    public ApiResponse mqAddUrl(@RequestBody MqJson mqJson) {
+    @PostMapping(value = "addPositionUrl")
+    public ApiResponse mqAddPositionUrl(@RequestBody MqJson mqJson) {
+        return positionUrlProducer.producerHandle(mqJson.getMsg());
+    }
 
-        return new ApiResponse(ResponseCode.FAILED, "添加Url失败，Url已存在");
+    @PostMapping(value = "/addPositionsUrlHtml", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ApiResponse mqAddPositionsUrlHtml(@RequestBody DataToMq dataToMq) {
+        JsonObject<DataToMq> RecrPageJson = new JsonObject();
+        String data = RecrPageJson.toJson(dataToMq);
+        return positionUrlProducer.producerHandle(data);
+    }
+
+    @PostMapping(value = "/addPositionDetailHtml", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ApiResponse mqAddPositionDetailHtml(@RequestBody DataToMq dataToMq) {
+        JsonObject<DataToMq> RecrPageJson = new JsonObject();
+        String data = RecrPageJson.toJson(dataToMq);
+        return positionUrlProducer.producerHandle(data);
     }
 }
