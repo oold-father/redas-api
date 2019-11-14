1194df7724859400e5e9909901e247c3ef3ba52c
Fifi Lyu <fifilyu@gmail.com>
2019-11-14 22:39:50 +0800

    fix: 解决Tomcat运行失败问题

--- src/main/java/com/cdgeekcamp/redas/api/core/RedasApiCoreApplication.java
+++ src/main/java/com/cdgeekcamp/redas/api/core/RedasApiCoreApplication.java
@@ -3,6 +3,8 @@ package com.cdgeekcamp.redas.api.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -13,8 +15,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
         "com.cdgeekcamp.redas.lib.core",
         "com.cdgeekcamp.redas.api.core"
 })
-public class RedasApiCoreApplication {
+public class RedasApiCoreApplication extends SpringBootServletInitializer {
+    // SpringBootServletInitializer以及SpringApplicationBuilder相关代码用于兼容Tomcat运行本项目
+
     public static void main(String[] args) {
-        SpringApplication.run(RedasApiCoreApplication.class);
+        SpringApplication.run(RedasApiCoreApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(RedasApiCoreApplication.class);
     }
 }
