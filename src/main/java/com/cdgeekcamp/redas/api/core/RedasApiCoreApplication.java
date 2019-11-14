5c78493e8c5150e031d82e6e26ff3fc22a1da8a2
Yinxin <yinxin666ccyy@gmail.com>
2019-11-14 13:56:09 +0800

    feat(application启动类): 添加war所需要的代码

--- src/main/java/com/cdgeekcamp/redas/api/core/RedasApiCoreApplication.java
+++ src/main/java/com/cdgeekcamp/redas/api/core/RedasApiCoreApplication.java
@@ -3,6 +3,8 @@ package com.cdgeekcamp.redas.api.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -13,7 +15,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
         "com.cdgeekcamp.redas.lib.core",
         "com.cdgeekcamp.redas.api.core"
 })
-public class RedasApiCoreApplication {
+public class RedasApiCoreApplication extends SpringBootServletInitializer {
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RedasApiCoreApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(RedasApiCoreApplication.class);
     }
