2cd33db3af9d26a0d68abac087840269cce8a3ba
wjcml <37792055+wjcml@users.noreply.github.com>
2019-12-05 19:31:26 +0800

    feat(<positionList>): 添加positionList的接口

---
+++ src/main/java/com/cdgeekcamp/redas/api/core/controller/PositionController.java
@@ -0,0 +1,47 @@
+package com.cdgeekcamp.redas.api.core.controller;
+
+import com.cdgeekcamp.redas.db.model.Position;
+import com.cdgeekcamp.redas.db.model.PositionRepository;
+import com.cdgeekcamp.redas.lib.core.api.ApiResponseX;
+import com.cdgeekcamp.redas.lib.core.api.ResponseCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+@RestController
+@RequestMapping(value = "/position")
+public class PositionController {
+    @Autowired
+    private PositionRepository positionRepository;
+
+    @GetMapping(value = "/getPositionList")
+    public ApiResponseX getPositionList(@PathParam("page") Integer page){
+        if(page == null || page <= 0){
+            page = 0;
+        }else {
+            page = page-1;
+        }
+        Pageable pageable = PageRequest.of(page, 20, Sort.Direction.ASC, "Id");
+        Page<Position> positions = positionRepository.findAll(pageable);
+
+        LinkedHashMap<String, Object> map = new LinkedHashMap<>();
+        map.put("totalPage", positions.getTotalPages());
+
+        ArrayList<Position> positionList = new ArrayList<>();
+        for (Position position : positions) {
+            positionList.add(position);
+        }
+        map.put("positionList", positionList);
+
+        return new ApiResponseX<>(ResponseCode.SUCCESS, "成功", map);
+    }
+}
