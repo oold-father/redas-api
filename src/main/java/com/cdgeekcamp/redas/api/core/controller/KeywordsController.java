0f3a9e1d237299423ce74873be91123766dadbe3
Yinxin <yinxin666ccyy@gmail.com>
2019-12-18 11:16:16 +0800

    feat(keyword): keywordsController

---
+++ src/main/java/com/cdgeekcamp/redas/api/core/controller/KeywordsController.java
@@ -0,0 +1,126 @@
+package com.cdgeekcamp.redas.api.core.controller;
+
+
+import com.cdgeekcamp.redas.db.model.KeyWords;
+import com.cdgeekcamp.redas.db.model.KeyWordsRepository;
+import com.cdgeekcamp.redas.db.model.Nature;
+import com.cdgeekcamp.redas.db.model.PositionUrl;
+import com.cdgeekcamp.redas.lib.core.api.ApiResponse;
+import com.cdgeekcamp.redas.lib.core.api.ApiResponseList;
+import com.cdgeekcamp.redas.lib.core.api.ApiResponseMap;
+import com.cdgeekcamp.redas.lib.core.api.ResponseCode;
+import org.elasticsearch.common.collect.HppcMaps;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.Null;
+import javax.websocket.server.PathParam;
+import java.util.*;
+import java.util.stream.StreamSupport;
+
+@RestController
+@RequestMapping(value = "/keyword")
+public class KeywordsController {
+    @Autowired
+    private KeyWordsRepository keyWordsRepository;
+
+    @GetMapping(value = "/get")
+    public ApiResponse getKeywordsInfo(@RequestParam(name = "page", defaultValue= "0", required=false) Integer page,
+                                       @RequestParam(name = "maxEle", defaultValue= "20", required=false) Integer maxEle) {
+
+        if(page <= 0){
+            page = 0;
+        }else {
+            page = page-1;
+        }
+
+        Pageable pageable = PageRequest.of(page, maxEle, Sort.Direction.ASC, "Id");
+        Page<KeyWords> keyWords = keyWordsRepository.findAll(pageable);
+
+//        Iterable<KeyWords> keyWords  = keyWordsRepository.findAll();
+        ArrayList<Object> keyWords_list = new ArrayList<>();
+        for (KeyWords item: keyWords){
+            Map<Object, Object> newNode = new HashMap<>();
+            newNode.put("id", item.getId());
+            newNode.put("name", item.getName());
+            if (item.getParentId() == null){
+                newNode.put("parent", "");
+                newNode.put("parent_id", null);
+            }else {
+                Optional<KeyWords> parent = keyWordsRepository.findById(item.getParentId());
+                newNode.put("parent", parent.get().getName());
+                newNode.put("parent_id", parent.get().getId());
+            }
+            keyWords_list.add(newNode);
+        }
+
+        ApiResponseMap<String, Object> responseMap = new ApiResponseMap<>(ResponseCode.SUCCESS, "查询成功");
+        responseMap.addKeyAndValue("totalElements", keyWords.getTotalElements());
+        responseMap.addKeyAndValue("data", keyWords_list);
+        return responseMap;
+    }
+
+    @PostMapping(value = "/update")
+    public ApiResponse updateKeywordsInfo(
+            @RequestParam(name = "id") Integer id,
+            @RequestParam(name = "name") String name,
+            @RequestParam(name = "parent_id") Integer parent_id
+            ) {
+        Optional<KeyWords> keyWords  = keyWordsRepository.findById(id);
+
+        if (keyWords.isEmpty()){
+            return new ApiResponse(ResponseCode.FAILED, "更新失败, id不存在");
+        }
+        KeyWords keyWord = keyWords.get();
+        keyWord.setName(name);
+
+        if (parent_id == 0){
+            parent_id = null;
+        }
+        keyWord.setParentId(parent_id);
+
+        keyWordsRepository.save(keyWord);
+        return new ApiResponse(ResponseCode.SUCCESS, "更新成功");
+    }
+
+    @PostMapping(value = "/add")
+    public ApiResponse addKeywordsInfo(
+            @RequestParam(name = "name") String name,
+            @RequestParam(name = "parent_id") Integer parent_id
+    ) {
+        Optional<KeyWords> keyWords  = keyWordsRepository.findByNameAndParentId(name, parent_id);
+
+        if (!keyWords.isEmpty()){
+            return new ApiResponse(ResponseCode.FAILED, "添加失败失败, 已存在该keyword");
+        }
+
+        KeyWords keyWord = new KeyWords(parent_id, name);
+        keyWordsRepository.save(keyWord);
+        return new ApiResponse(ResponseCode.SUCCESS, "更新成功");
+    }
+
+    @PostMapping(value = "/delete")
+    public ApiResponse deleteKeywordsInfo(
+            @RequestParam(name = "id") Integer id
+    ) {
+        Optional<KeyWords> keyWords  = keyWordsRepository.findById(id);
+
+        if (keyWords.isEmpty()){
+            return new ApiResponse(ResponseCode.FAILED, "删除失败, id不存在");
+        }
+
+        Iterable<KeyWords> childKey  = keyWordsRepository.findByParentId(id);
+
+        if (0 != StreamSupport.stream(childKey.spliterator(), false).count()){
+            return new ApiResponse(ResponseCode.FAILED, "删除失败, 该字段存在有子字段, 无法删除");
+        }
+
+        keyWordsRepository.deleteById(id);
+        return new ApiResponse(ResponseCode.SUCCESS, "删除成功");
+    }
+
+}
